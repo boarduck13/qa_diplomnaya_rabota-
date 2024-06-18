d048b8545bc17bf970f9b2c07871d36cee5d75b3
Andrey <justupor@gmail.com>
2024-06-18 17:28:51 +0300

    Add CardInfo

---
+++ src/test/java/DataHelper/CardInfo.java
@@ -0,0 +1,38 @@
+package DataHelper;
+
+public class CardInfo {
+
+    String number;
+    String mouth;
+    String year;
+    String owner;
+    String cvc;
+
+    public CardInfo(String number, String mouth, String year, String owner, String cvc) {
+        this.number = number;
+        this.mouth = mouth;
+        this.year = year;
+        this.owner = owner;
+        this.cvc = cvc;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getMouth() {
+        return mouth;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public String getCvc() {
+        return cvc;
+    }
+}
